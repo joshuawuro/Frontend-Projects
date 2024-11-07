34fd4be38039d146c5423e1e23c5291ae463f9d0
Joshuawuro <joshuawuro@gmail.com>
2024-11-07 20:57:11 +0000

    navbar

--- GITHUB_USER_API/src/components/common/Navbar.jsx
+++ GITHUB_USER_API/src/components/common/Navbar.jsx
@@ -1,7 +1,9 @@
 function Navbar() {
   return (
     <>
-      <h2>GITHUB USER SEARCH</h2>
+      <h2 className="py-20 max-w-[1240px] mx-auto text-center text-5xl font-bold">
+        GITHUB USER SEARCH
+      </h2>
     </>
   );
 }
