5e721d15c5b78a173fbe2f0db8d8c2157b1ab98f
Joshuawuro <joshuawuro@gmail.com>
2024-11-06 22:31:17 -0800

    chore: routing setup

--- GITHUB_USER_API/src/App.jsx
+++ GITHUB_USER_API/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/common/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
+const {
+  RouterProvider,
+  createBrowserRouter,
+  Outlet,
+} = require("react-router-dom");
 
+const Root = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Outlet />
     </>
-  )
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    root: "/",
+    element: <Root />,
+    children: {},
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
